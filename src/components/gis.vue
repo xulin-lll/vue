590a11e24d9d99fb95acb72c5ca945d27ac43616
xulin-lll <603846490@qq.com>
2018-06-07 15:55:39 +0800

    first commit

---
+++ src/components/gis.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="gismap" v-initmap>
+  </div>
+</template>
+<script>
+import Map from "ol/map";
+import View from "ol/view";
+import TileLayer from "ol/layer/tile";
+import XYZ from "ol/source/xyz";
+import "ol/ol.css";
+
+export default {
+  name: "gismap",
+  directives: {
+    initmap: {
+      inserted: function() {
+        new Map({
+          target: "gismap",
+          layers: [
+            new TileLayer({
+              source: new XYZ({
+                url: "https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              })
+            })
+          ],
+          view: new View({
+            center: [0, 0],
+            zoom: 2
+          })
+        });
+      }
+    }
+  }
+};
+</script>
